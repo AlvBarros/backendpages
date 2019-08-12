1e2f0abeac23d2b52b17c43ce74f00aa6e9a1a2d
AlvBarros <alv.barrosc@hotmail.com>
2019-08-12 20:55:06 -0300

    tutorial for database configuration

--- src/configuration/template/Cloudant.ts
+++ src/configuration/template/Cloudant.ts
@@ -29,3 +29,36 @@ export class Cloudant {
 }
 
 export default Cloudant;
+/*
+
+test.index(function(err, result) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('The database has %d indexes', result.indexes.length);
+  for (var i = 0; i < result.indexes.length; i++) {
+    console.log('  %s (%s): %j', result.indexes[i].name, result.indexes[i].type, result.indexes[i].def);
+  }
+});
+
+var queryById = { name:'queryById', type:'json', index:{fields:['id'] }}
+test.index(queryById, function(err, response) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Index creation result: %s', response.result);
+});
+
+test.find({ selector: { id:1 } }, function(err, result) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Found %d documents with id 1', result.docs.length);
+  for (var i = 0; i < result.docs.length; i++) {
+    console.log('  Doc id: %s', result.docs[i]._id);
+  }
+});
+*/
